eebd74950904b8605ea0e07aa3de6da06c79d836
Elisama O Santos <elisamaufrj@gmail.com>
2024-01-23 12:05:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("myFrame").onload = function() {myFunction()};
+
+function myFunction() {
+        document.getElementById("showDate").innerHTML = "Iframe is loaded.";
+      }
