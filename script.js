91906903a12d1bb46f1fe79ac8b7c63da69ecdf1
Elisama O Santos <elisamaufrj@gmail.com>
2024-01-23 12:08:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-document.getElementById("myFrame").onload = function() {myFunction()};
-
 function myFunction() {
         document.getElementById("showDate").innerHTML = "Iframe is loaded.";
-      }
+}
